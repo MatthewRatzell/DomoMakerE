c3a4f7c422482db619b48564554fb76808c69ae2
Matthew Ratzell <mjr4427@g.rit.edu>
2022-04-03 17:21:35 -0400

    fixed linting errors

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -5,23 +5,21 @@ const { Domo } = models;
 
 const makerPage = (req, res) => res.render('app');
 
-//https://www.kindacode.com/article/how-to-easily-generate-a-random-string-in-node-js/
-const generateRandomString = (myLength) => {
-  const chars =
-    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-  const randomArray = Array.from(
-    { length: myLength },
-    (v, k) => chars[Math.floor(Math.random() * chars.length)]
-  );
+// https://www.codegrepper.com/code-examples/javascript/node+js+generate+random+string
+function makeid() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-  const randomString = randomArray.join("");
-  return randomString;
-};
-//https://futurestud.io
-function between(min, max) {  
+  for (let i = 0; i < 5; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+// https://futurestud.io
+function between(min, max) {
   return Math.floor(
-    Math.random() * (max - min) + min
-  )
+    Math.random() * (max - min) + min,
+  );
 }
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age || !req.body.height) {
@@ -55,12 +53,11 @@ const getDomos = (req, res) => DomoModel.findByOwner(req.session.account._id, (e
   return res.json({ domos: docs });
 });
 
-
-const addRandomDomo = async (req,res) => {
+const addRandomDomo = async (req, res) => {
   const domoData = {
-    name: generateRandomString(5),
-    age: between(0,10),
-    height: between(50,100),
+    name: makeid(),
+    age: between(0, 10),
+    height: between(50, 100),
     owner: req.session.account._id,
   };
 
@@ -74,7 +71,6 @@ const addRandomDomo = async (req,res) => {
     }
     return res.status(400).json({ error: 'An error occured' });
   }
-
 };
 
 module.exports = {
